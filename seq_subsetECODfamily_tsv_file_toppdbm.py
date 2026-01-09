1b76db844ada811ad746a09096814aca30a0ddb5
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-01-09 15:22:57 +1300

    Update seq_subsetECODfamily_tsv_file_toppdbm.py

--- seq_subsetECODfamily_tsv_file_toppdbm.py
+++ seq_subsetECODfamily_tsv_file_toppdbm.py
@@ -1,47 +1,3 @@
-#goal of this code is to parse and categorize families.
-#using the ecod database
-
-ecod_info_dict = {}
-with open('ecod.v293.domains.txt', 'r') as lines:
-    for line in lines:
-        if line[0] == '#':
-            pass
-        elif 'pdb_chain' in line:
-            #this is to remove the header
-            pass
-        else:
-            #sometimes  multiple components make up a pdb id so it is split up
-            if '-' in line.split('\t')[5]:
-                multimer_split = []
-                multimer_split = line.split('\t')[5].split('-')
-                for mer in multimer_split:
-                    pdb_id = line.split('\t')[4] + '_' + mer
-                    pdb_id = (line.split('\t')[4] + '_' + line.split('\t')[5])
-                
-                #ecod has its own family classification, firs part is architecture of structure (beta-barrel), second is structural similarity but little homology support. 
-                #third is second but grouped based on topological connections, the last is the family (Often pfam). 
-    
-                #so for 2nmz_A, its family_id = 1.1.1.3, 1 1 (structural similarity). = cradle loop barrel, 
-                #1 expected homology = RIFT-related, 1 topological connection = Acid protease, 3 family = RVP. 
-                family_id = line.split('\t')[3]
-                #I also append the structural architecture to this.
-                Architecture = line.split('\t')[7]
-                #a family id 
-                f_name = line.split('\t')[11]
-                info_list = (pdb_id,family_id, f_name, Architecture)
-                ecod_info_dict[pdb_id] = info_list
-            #if it just the idenvidual chain then we write it like this:      
-            else:
-                pdb_id = (line.split('\t')[4] + '_' + line.split('\t')[5])
-                
-                family_id = line.split('\t')[3]
-                
-                Architecture = line.split('\t')[7]
-                
-                f_name = line.split('\t')[11]
-                #concatenate all the info and save into dictionairy.
-                info_list = (pdb_id,family_id, f_name, Architecture)
-                ecod_info_dict[pdb_id] = info_list
 #this function (in a laborius manner) calculates the relative frequency for each amino acid 
 def return_line_of_frequencies(input_list):
     #the 0.000000000001, addition is done to ensure the calculation still work despite some amino acids possibly being absent
@@ -72,10 +28,19 @@ def return_line_of_frequencies(input_list):
     K  + '\t' +  M  + '\t' +  F  + '\t' +  P  + '\t' +  S  + '\t' +  T  + '\t' +  W  + '\t' +  Y  + '\t' +  V 
     return(line)
 
+#subset data in membrane localizations
 
 import os
 from Bio import SeqIO
 import json
+
+
+
+membrane_data = '/nesi/nobackup/uc04105/PDB_alpha/pdb_chain_id_mebrane_data.json'
+with open('ecod_data.json') as json_data:
+    ecod_info_dict = json.load(json_data)
+    json_data.close()
+
 
 writing_dir = '/nesi/nobackup/uc04105/PDB_alpha/tma_topdb/family_sep'
 #parse the pdb ids present in the filtered pdbtm database
@@ -121,7 +86,7 @@ with open('/nesi/nobackup/uc04105/PDB_alpha/Alpha-Helical_tm_PDBTM_ECOD_family.t
             output.close()
             
             with open('/nesi/nobackup/uc04105/PDB_alpha/fl_topdb/family_sep/{}_fl.fasta'.format(family_name), 'a') as output:
-                for record in SeqIO.parse('/nesi/nobackup/uc04105/PDB_alpha/fl_topdb/{}'.format(pdb_chain_fasta.replace('tm', 'fl')), 'fasta'):
+                for record in SeqIO.parse('/nesi/nobackup/uc04105/PDB_alpha/fl_topdb/{}'.format(pdb_chain_fasta.replace('_tm', '_fl')), 'fasta'):
                     fasta_line = '>' + record.id + ' family_id: {} ecod_id: {}'.format(family_name, ecod_code) + '\n' + str(record.seq) + '\n'
                     fl_seq = str(record.seq)
 
@@ -135,11 +100,13 @@ with open('/nesi/nobackup/uc04105/PDB_alpha/Alpha-Helical_tm_PDBTM_ECOD_family.t
                 output.write(fasta_line)
             output.close()
             Architecture = ecod_info_dict[pdb_chain_fasta.split('_tm.fasta')[0]][-1]
+
             
             if pdb_chain_fasta.split('_tm.fasta')[0] in membrane_dict:
                 
                 tsv_line = pdb_chain_fasta.split('_tm.fasta')[0] + '\t' + family_name + '\t' + Architecture + '\t' + ecod_code + '\t' + tm_seq + '\t' + str(len(tm_seq)) + '\t' + fl_seq + '\t' + str(len(fl_seq)) + '\t'  + membrane_dict[pdb_chain_fasta.split('_tm.fasta')[0]].replace(' ', '_') + '\t' + count_aa_tm + '\t' + count_aa_fl +'\n'
-                print(tsv_line)
+                tsv_out.write(tsv_line)
             else:
-                pass
-            tsv_out.write(tsv_line)
+                tsv_line = pdb_chain_fasta.split('_tm.fasta')[0] + '\t' + family_name + '\t' + Architecture + '\t' + ecod_code + '\t' + tm_seq + '\t' + str(len(tm_seq)) + '\t' + fl_seq + '\t' + str(len(fl_seq)) + '\t'  + 'non-called' + '\t' + count_aa_tm + '\t' + count_aa_fl +'\n'
+
+                tsv_out.write(tsv_line)

4fc322cfd66e2c238b86e74e1e4802a3ae2c7c85
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-01-13 06:54:21 +1300

    Update rel_aminoacid_freq_per_membranetype.py

--- rel_aminoacid_freq_per_membranetype.py
+++ rel_aminoacid_freq_per_membranetype.py
@@ -2,12 +2,11 @@ import json
 from Bio import SeqIO
 
 
-
-header = 'Name' + '\t' + 'total_aa_count' + '\t' + 'A'+ '\t' + 'R' + '\t' + 'N'+ '\t' + 'D'+ '\t' + 'C'+ '\t' + \
+import os
+header = 'Name' + '\t' + 'total_aa_count' + '\t' + 'type' + '\t' + 'A'+ '\t' + 'R' + '\t' + 'N'+ '\t' + 'D'+ '\t' + 'C'+ '\t' + \
 'Q'+ '\t' + 'E'+ '\t' + 'G'+ '\t' + 'H'+ '\t' + 'I'+ '\t' + 'L'+ '\t' + 'K'+ '\t' + 'M'+ '\t' + \
 'F'+ '\t' +'P'+ '\t' + 'S'+ '\t' + 'T'+ '\t' + 'W'+ '\t' + 'Y'+ '\t' + 'V' + '\n'
 
-print(len(amino_acids_single))
 fl_dir = '/nesi/nobackup/uc04105/PDB_alpha/fl_topdb'
 atm_dir = '/nesi/nobackup/uc04105/PDB_alpha/tma_topdb'
 
@@ -17,30 +16,30 @@ def return_line_of_frequencies(input_list, name):
     #the 0.000000000001, addition is done to ensure the calculation still work despite some amino acids possibly being absent
     total = len(input_list)
     if total != 0:
-        A = str((input_list.count('A') + 0.000000000001)/total)
-        R = str((input_list.count('R') + 0.000000000001)/total)
-        N = str((input_list.count('N') + 0.000000000001)/total)
-        D = str((input_list.count('D') + 0.000000000001)/total)
-        C = str((input_list.count('C') + 0.000000000001)/total)
-        Q = str((input_list.count('Q') + 0.000000000001)/total)
-        E = str((input_list.count('E') + 0.000000000001)/total)
-        G = str((input_list.count('G') + 0.000000000001)/total)
-        H = str((input_list.count('H') + 0.000000000001)/total)
-        I = str((input_list.count('I') + 0.000000000001)/total)
-        L = str((input_list.count('L') + 0.000000000001)/total)
-        K = str((input_list.count('K') + 0.000000000001)/total)
-        M = str((input_list.count('M') + 0.000000000001)/total)
-        F = str((input_list.count('F') + 0.000000000001)/total)
-        P = str((input_list.count('P') + 0.000000000001)/total)
-        S = str((input_list.count('S') + 0.000000000001)/total)
-        T = str((input_list.count('T') + 0.000000000001)/total)
-        W = str((input_list.count('W') + 0.000000000001)/total)
-        Y = str((input_list.count('Y') + 0.000000000001)/total)
-        V = str((input_list.count('V') + 0.000000000001)/total)
+        A = str((input_list.count('A') + 1))
+        R = str((input_list.count('R') + 1))
+        N = str((input_list.count('N') + 1))
+        D = str((input_list.count('D') + 1))
+        C = str((input_list.count('C') + 1))
+        Q = str((input_list.count('Q') + 1))
+        E = str((input_list.count('E') + 1))
+        G = str((input_list.count('G') + 1))
+        H = str((input_list.count('H') + 1))
+        I = str((input_list.count('I') + 1))
+        L = str((input_list.count('L') + 1))
+        K = str((input_list.count('K') + 1))
+        M = str((input_list.count('M') + 1))
+        F = str((input_list.count('F') + 1))
+        P = str((input_list.count('P') + 1))
+        S = str((input_list.count('S') + 1))
+        T = str((input_list.count('T') + 1))
+        W = str((input_list.count('W') + 1))
+        Y = str((input_list.count('Y') + 1))
+        V = str((input_list.count('V') + 1))
     else:
         A  =  R  = N  = D  =  C  =  Q  =  E  = G  =  H  =  I  = L  = K =  M  = F  =  P =  S =  T  = W =  Y  =  V  = 'NaN'
         
-    line = name + '\t' + str(total) +  '\t' + A  + '\t' +  R  + '\t' +  N  + '\t' + D  + '\t' + \
+    line = name + '\t' + str(total) +  '\t' +  name.split('_')[-1] + '\t' + A  + '\t' +  R  + '\t' +  N  + '\t' + D  + '\t' + \
     C  + '\t' +  Q  + '\t' +  E  + '\t' +  G  + '\t' +  H  + '\t' +  I  + '\t' +  L  + '\t' + \
     K  + '\t' +  M  + '\t' +  F  + '\t' +  P  + '\t' +  S  + '\t' +  T  + '\t' +  W  + '\t' +  Y  + '\t' +  V  + '\n'
     return(line)
@@ -51,6 +50,17 @@ with open('pdb_chain_id_mebrane_data.json', 'r', encoding='utf-8') as file:
         # Use json.load() to parse the file content into a Python object
         membrane_data = json.load(file)
 
+from collections import Counter
+
+def subtract_with_counts(list_a, list_b):
+    counts = Counter(list_b)
+    result = []
+    for item in list_a:
+        if counts[item] > 0:
+            counts[item] -= 1
+        else:
+            result.append(item)
+    return result
 
 
 #make a set list of all membrane types
@@ -106,7 +116,8 @@ for file in os.listdir(fl_dir):
             for i in str(record.seq):
                 membrane_freqfl_library[membrane_type].append(i)
         
-        #open the right key in the membrane_freq_library dictionary
+        #open the right key in the membranefrom collections import Counter
+
 fl_aa_list = []
 #merge all the fl files
 
@@ -115,7 +126,7 @@ for record in SeqIO.parse('{}/all_fl_merged.faa'.format(fl_dir), 'fasta'):
         fl_aa_list.append(aa)
 
 #calculate the amino acid frequency
-name = 'fl_tm_proteins'
+name = 'merged_fl'
 fl_line =  return_line_of_frequencies(fl_aa_list, name)
 
 atm_aa_list = []
@@ -126,23 +137,40 @@ for record in SeqIO.parse('{}/all_atm_merged.faa'.format(atm_dir), 'fasta'):
     for aa in str(record.seq):
         atm_aa_list.append(aa)
 
+
 #calculate the amino acid frequency
 
-name = 'atm_tm_proteins'
+name = 'merged_tm'
 atm_line = (return_line_of_frequencies(atm_aa_list, name))
+
+
+#subtract lists from eachother
+non_membrane_regions_aa_list = subtract_with_counts(fl_aa_list, atm_aa_list)
+print(len(non_membrane_regions_aa_list))
+print(len(fl_aa_list))
+print(len(atm_aa_list))
+
+
+name = 'merged_nontm'
+non_mem_line = (return_line_of_frequencies(non_membrane_regions_aa_list, name))
+
 
 import os
 with open('relative_freq_aa_atm_fl.tsv', 'w') as X:
     X.write(header)
     X.write(fl_line)
     X.write(atm_line)
-
+    X.write(non_mem_line)
     
     for key in membrane_freqfl_library.keys():
         name = key + '_fl'
-        input_list = membrane_freqfl_library[key]
-        X.write(return_line_of_frequencies(input_list, name))
-    for key in membrane_freqtm_library.keys():
+        input_fl_list = membrane_freqfl_library[key]
+        X.write(return_line_of_frequencies(input_fl_list, name))
+        
         name = key + '_tm'
-        input_list = membrane_freqtm_library[key]
-        X.write(return_line_of_frequencies(input_list, name))
+        input_atm_list = membrane_freqtm_library[key]
+        X.write(return_line_of_frequencies(input_atm_list, name))
+
+        name = key + '_nontm'
+        non_tm_list = subtract_with_counts(input_fl_list, input_atm_list)
+        X.write(return_line_of_frequencies(non_tm_list, name))
